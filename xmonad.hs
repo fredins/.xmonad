d328eecc20c13bf6e3bbe11e6e6426591e34f8eb
Martin Fredin <fredin.martin@gmail.com>
2022-10-18 15:56:58 +0200

    Add dynamic workspaces

--- xmonad.hs
+++ xmonad.hs
@@ -1,54 +1,55 @@
-{-# LANGUAGE NamedFieldPuns #-}
-import           Control.Monad                    (when)
-import           Data.Foldable                    (traverse_)
-import qualified Data.Map                         as M
-import           Data.Maybe                       (fromJust)
-import           Data.Monoid                      (All (All))
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE NamedFieldPuns      #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+import           Control.Monad                       (when)
+import           Data.Foldable                       (traverse_)
+import qualified Data.Map                            as M
+import           Data.Maybe                          (fromJust, isNothing)
+import           Data.Monoid                         (All (All))
 import           IfMaxAlt
 import           Prelude
 import           XMonad
-import           XMonad.Actions.CopyWindow        (copyToAll,
-                                                   killAllOtherCopies)
+import           XMonad.Actions.CopyWindow           (copyToAll,
+                                                      killAllOtherCopies)
 import           XMonad.Actions.CycleWS
-import           XMonad.Actions.Minimize          (maximizeWindow,
-                                                   maximizeWindowAndFocus,
-                                                   minimizeWindow,
-                                                   withLastMinimized)
+import           XMonad.Actions.Minimize             (maximizeWindow,
+                                                      maximizeWindowAndFocus,
+                                                      minimizeWindow,
+                                                      withLastMinimized)
 import           XMonad.Config.Desktop
 import           XMonad.Hooks.EwmhDesktops
-import           XMonad.Hooks.InsertPosition      (Focus (Newer),
-                                                   Position (End),
-                                                   insertPosition)
-import           XMonad.Hooks.ManageDocks         (ToggleStruts (ToggleStruts),
-                                                   avoidStruts, docks)
-import           XMonad.Hooks.Minimize            (minimizeEventHook)
+import           XMonad.Hooks.InsertPosition         (Focus (Newer),
+                                                      Position (End),
+                                                      insertPosition)
+import           XMonad.Hooks.ManageDocks            (ToggleStruts (ToggleStruts),
+                                                      avoidStruts, docks)
+import           XMonad.Hooks.Minimize               (minimizeEventHook)
 import           XMonad.Hooks.Place
 import           XMonad.Hooks.SetWMName
-import           XMonad.Layout.BoringWindows      (BoringWindows, boringWindows,
-                                                   focusDown, focusMaster,
-                                                   focusUp)
-import           XMonad.Layout.ButtonDecoration
-import           XMonad.Layout.DecorationAddons
-import           XMonad.Layout.DecorationMadness
+import           XMonad.Layout.BoringWindows         (boringWindows, focusDown,
+                                                      focusMaster, focusUp)
+import           XMonad.Layout.ImageButtonDecoration (defaultThemeWithImageButtons)
 import           XMonad.Layout.Maximize
-import           XMonad.Layout.Minimize           (minimize)
-import           XMonad.Layout.NoFrillsDecoration (noFrillsDeco)
-import           XMonad.Layout.ResizableTile      (MirrorResize (..),
-                                                   ResizableTall (..))
+import           XMonad.Layout.Minimize              (minimize)
+import           XMonad.Layout.ResizableTile         (MirrorResize (..),
+                                                      ResizableTall (..))
+import           XMonad.Layout.SideBorderDecoration
 import           XMonad.Layout.SimpleDecoration
-import           XMonad.Layout.Simplest           (Simplest (Simplest))
-import           XMonad.Layout.Spacing
-import qualified XMonad.StackSet                  as W
-import           XMonad.StackSet                  (integrate', peek, stack)
+import           XMonad.Layout.Simplest              (Simplest (Simplest))
+import qualified XMonad.StackSet                     as W
+import           XMonad.StackSet                     (integrate', peek, stack)
 import           XMonad.Util.Cursor
 import           XMonad.Util.Themes
-
+import           XMonad.Util.WorkspaceCompare        (WorkspaceSort,
+                                                      filterOutWs)
 main = xmonad
      . ewmhFullscreen
+     . addEwmhWorkspaceSort (filterEmpty <$> gets windowset)
      . ewmh
      . docks
-     $ desktopConfig
+     $ conf
 
+conf = desktopConfig
      { terminal           = "alacritty"
      , modMask            = mod4Mask
      , startupHook        = setWMName "LG3D" >> setDefaultCursor xC_left_ptr
@@ -65,16 +66,16 @@ main = xmonad
 layoutHook' =
   let
     tiled = ResizableTall 1 (1 / 20) (103 / 200) []
-    decor = buttonDeco shrinkText defaultThemeWithButtons
-    -- decor = simpleDeco shrinkText (theme wfarrTheme)
+    decor (x :: l Window) = sideBorderLayout (sideBorderConf conf) x
+    -- decor = imageButtonDeco clearlooks
   in
     avoidStruts
   . maximizeWithPadding 0
   . minimize
   . boringWindows
---  . spacingRaw True (Border 0 0 0 0) False (Border 0 0 8 0) True
   . ifMax 1 Simplest
-  $ decor tiled ||| Full ||| decor (Mirror tiled)
+  . decor
+  $ tiled ||| Full ||| Mirror tiled
 
 handleEventHook' :: Event -> X All
 handleEventHook' = composeAll
@@ -82,23 +83,49 @@ handleEventHook' = composeAll
   , showDesktopEventHook
   ]
 
-
 manageHook' :: ManageHook
 manageHook' = composeAll
   [ placeHook $ smart (0.5, 0.5)
-  , className =? "Animator" --> doFloat
+  , className =? "Floating" --> doFloat
   , insertPosition End Newer
   ]
 
+filterEmpty :: WindowSet -> WorkspaceSort
+filterEmpty ss =
+  let
+    isEmpty w = isNothing (W.stack w)
+             && W.currentTag ss /= W.tag w
+  in
+    filterOutWs
+  . map W.tag
+  . filter isEmpty
+  $ W.workspaces ss
+
 showDesktopEventHook :: Event -> X All
-showDesktopEventHook ClientMessageEvent{ev_message_type} = do
-  x <- getAtom "_NET_SHOWING_DESKTOP"
-  when (x == ev_message_type) showDesktop
-  return (All True)
+showDesktopEventHook = \case
+  ClientMessageEvent{ev_message_type} -> do
+    x <- getAtom "_NET_SHOWING_DESKTOP"
+    when (x == ev_message_type) showDesktop
+    pure $ All True
+  _ ->
+    pure $ All True
+
+sideBorderConf :: XConfig a -> SideBorderConfig
+sideBorderConf conf = def
+  { sbSide = U
+  , sbActiveColor = XMonad.focusedBorderColor conf
+  , sbInactiveColor = XMonad.normalBorderColor conf
+  , sbSize = 7
+  }
+
+clearlooks :: Theme
+clearlooks = (theme wfarrTheme)
+  { windowTitleIcons = windowTitleIcons defaultThemeWithImageButtons }
 
 myKeys conf@XConfig { XMonad.modMask = modm } =
   M.fromList
     $  [ ((modm, xK_n)                  , spawn $ XMonad.terminal conf)
+       , ((modm .|. shiftMask, xK_n)    , spawn $ XMonad.terminal conf <> " --working-directory $(xcwd)")
        , ((modm, xK_o)                  , withWindowSet dwmZero)
        , ((modm, xK_p)                  , spawn "dmenu_run")
        , ((modm, xK_c)                  , spawn "clipmenu")
@@ -106,7 +133,7 @@ myKeys conf@XConfig { XMonad.modMask = modm } =
        , ((modm .|. shiftMask, xK_c)    , kill)
        , ((modm, xK_space)              , sendMessage NextLayout)
        , ((modm .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
-       , ((modm .|. shiftMask, xK_n)    , refresh)
+       , ((modm .|. shiftMask, xK_r)    , refresh)
        , ((modm, xK_j)                  , focusDown)
        , ((modm, xK_k)                  , focusUp)
        , ((modm, xK_BackSpace)          , focusMaster)
@@ -137,7 +164,6 @@ myKeys conf@XConfig { XMonad.modMask = modm } =
        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
        , (f, m) <- [(lazyView, 0), (W.shift, shiftMask)]]
 
-
 showWindows :: X ()
 showWindows = traverseWindows maximizeWindow
 
@@ -147,9 +173,7 @@ showDesktop = traverseWindows minimizeWindow
 traverseWindows :: (Window -> X a) -> X ()
 traverseWindows f = withWindowSet
                   $ traverse_ f
-                  . concatMap (integrate' . stack)
-                  . W.workspaces
-
+                  . W.allWindows
 
 dwmZero :: WindowSet -> X ()
 dwmZero w = toggle $ filter (== (fromJust . peek) w) ws
@@ -163,23 +187,4 @@ dwmZero w = toggle $ filter (== (fromJust . peek) w) ws
 isVisible w ws = any ((w ==) .  W.tag . W.workspace) (W.visible ws)
 lazyView w ws | isVisible w ws = ws
               | otherwise      = W.view w ws
-
-
-{-
-greedyView :: (Eq s, Eq i) => i -> W.StackSet i l a s sd -> W.StackSet i l a s sd
-greedyView w ws
-     | any wTag (W.hidden ws) = W.view w ws
-     | (Just s) <- find (wTag . W.workspace) (W.visible ws)
-                            = ws { W.current = (W.current ws) { W.workspace = W.workspace s }
-                                 , W.visible = s { W.workspace = W.workspace (W.current ws) }
-                                           : filter (not . wTag . W.workspace) (W.visible ws) }
-     | otherwise = ws
-   where wTag = (w == ) . W.tag
-
-
-addWorkspaceAt :: (WindowSpace -> [WindowSpace] -> [WindowSpace]) -> String -> X ()
-addWorkspaceAt add newtag = addHiddenWorkspaceAt add newtag >> windows (greedyView newtag)
--}
-
-
 
